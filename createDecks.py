cac4d455f43bd53d8e185c5e01021dc65c2081d8
Spencer-Janelle <67770722+Spencer-Janelle@users.noreply.github.com>
2020-07-11 16:52:16 -0400

    Update createDecks.py

--- createDecks.py
+++ createDecks.py
@@ -4,3 +4,9 @@ import bpy, sys, os
 csvPath = sys.argv[-1]
 
 if os.path.exists(csvPath):
+  # open file in read mode
+with open('students.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
