b4d75bf536e72d3bddbb05e9dec800ea2afa1dc6
Spencer-Janelle <67770722+Spencer-Janelle@users.noreply.github.com>
2020-07-15 18:41:57 -0400

    Update createDecks.py

--- createDecks.py
+++ createDecks.py
@@ -25,19 +25,19 @@ if os.path.exists(csvPath):
         # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
         # Iterate over each row in the csv using reader object
-        bpy.ops.object.select_by_type(type='MESH')
-        bpy.ops.object.delete()
-        bpy.ops.object.text_add()
+
         for row in csv_reader:
             # take name and room number to create text
+            bpy.ops.object.delete()
+            bpy.ops.object.text_add(location=(0,0,0), rotation=(0,0,0))
             obj = bpy.context.object
             print(row[0])
             obj.data.body = row[0]
-
             # extrude
             obj.data.extrude = 0.2
             # texture/color
             obj.color = get_random_color()
-
+            bpy.ops.object.convert(target="MESH")
             # export as gltf
             bpy.ops.export_scene.gltf(export_format='GLTF_EMBEDDED', filepath=os.path.join(outputPath, row[0]))
+
