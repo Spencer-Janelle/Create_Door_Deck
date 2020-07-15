b63094667ac6a57cff50819a11ee7b69c5b6241d
Spencer-Janelle <67770722+Spencer-Janelle@users.noreply.github.com>
2020-07-15 16:21:13 -0400

    Update createDecks.py

--- createDecks.py
+++ createDecks.py
@@ -1,29 +1,37 @@
 # Run as: blender -b <filename> -P <this_script> -- <csv_path>
-import bpy, sys, os
-#Assume the last argument is csv path
-csvPath = sys.argv[-1]
+from _csv import reader
+from random import random
 
-if os.path.exists(csvPath):
-  # open file in read mode
-  with open('students.csv', 'r') as read_obj:
-    # pass the file object to reader() to get the reader object
-    csv_reader = reader(read_obj)
-    # Iterate over each row in the csv using reader object
-    bpy.ops.object.text_add()
-    for row in csv_reader:
-        # take name and room number to create text
-      obj=bpy.context.object
-      obj.data.body = row
-      
-      #extrude
-      
-      #texture/color
-      obj.color = get_random_color()
-      
-      #export as gltf
-      bpy.ops.export_scene.gltf('GLTF_EMBEDDED', filepath=row + ".gltf")
-      
-#random color for text      
+import bpy
+import os
+import sys
+
+
+# random color for text
 def get_random_color():
     r, g, b = [random.random() for i in range(3)]
     return r, g, b, 1
+
+
+# Assume the last argument is csv path
+csvPath = sys.argv[-1]
+
+if os.path.exists(csvPath):
+    # open file in read mode
+    with open(csvPath, 'r') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        bpy.ops.object.text_add()
+        for row in csv_reader:
+            # take name and room number to create text
+            obj = bpy.context.object
+            obj.data.body = row
+
+            # extrude
+            obj.data.extrude = 0.2
+            # texture/color
+            obj.color = get_random_color()
+
+            # export as gltf
+            bpy.ops.export_scene.gltf('GLTF_EMBEDDED', filepath=os.path.join('C:\Users\spenc\Desktop\gltf', row + '.gltf')
